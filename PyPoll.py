9239009d11837280f306dc277ba770c9b8013a64
gillranvir <gillranvir@outlook.com>
2021-01-17 15:18:17 -0500

    Adding the election analysis

--- PyPoll.py
+++ PyPoll.py
@@ -1,10 +1,4 @@
-import datetime
-
 import csv
-
-import random
-
-import numpy
 
 import os
 
@@ -63,77 +57,87 @@ with open(file_to_load) as election_data:
         # Add a vote to that candidate's count
         candidate_votes[candidate_name] += 1
 
-# 3. Print the total votes
-print(total_votes)
+# Save the results to our text file
+with open(file_to_save,'w') as txt_file:
+    # Print the final vote count to the terminal
+    election_results = (
+        f"\nElection Results\n"
+        f'-----------------------------\n'
+        f'Total Votes: {total_votes:,}\n'
+        f'-----------------------------\n')
+    print(election_results, end='')
 
-# Print the candidate list
-print(candidate_options)
+    # Save the final vote count to the text file
+    txt_file.write(election_results)
 
-# Print the candidate_vote dictionary
-print(candidate_votes)
+    # Iterate through the candidate list
+    for candidate_name in candidate_votes:
+        
+        # Retrieve vote count of a candidate
+        votes = candidate_votes[candidate_name]
 
-# Determine the percentage of votes for each candidate by looping through the counts
-# Iterate through the candidate list
-for candidate_name in candidate_votes:
-    
-    # Retrieve vote count of a candidate
-    votes = candidate_votes[candidate_name]
+        # Calculate percentage of votes
+        vote_percentage = float(votes) / float(total_votes) * 100
 
-    # Calculate percentage of votes
-    vote_percentage = float(votes) / float(total_votes) * 100
+        # Print each candidate's name, vote count and percentage of vote's 
+        candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
 
-    # Print candidate name and percentage of votes
-    # print(f"{candidate_name}: recieved {vote_percentage:.1f}% of the vote.")
+        # Print each candidate, their voter count, and percentage to the terminal.
+        print(candidate_results)
 
-    # Print each candidate's name, vote count and percentage of vote's 
-    print(f'{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n')
+        #  Save the candidate results to our text file.
+        txt_file.write(candidate_results)
 
-    # Determine winning vote and candidate
-    # Determine if the votes is greater than winning count
-    if (votes > winning_count) and (vote_percentage > winning_percentage):
-        # If true then set winning_count = votes and winning_percent = vote_percentage
-        winning_count = votes
-        winning_percentage = vote_percentage
-        # And, set the winning_candidate equal to the candidate's name
-        winning_candidate = candidate_name
+        # Determine winning vote and candidate
+        # Determine if the votes is greater than winning count
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            # If true then set winning_count = votes and winning_percent = vote_percentage
+            winning_count = votes
+            winning_percentage = vote_percentage
+            # And, set the winning_candidate equal to the candidate's name
+            winning_candidate = candidate_name
 
-winning_candidate_summary = (
-    f"----------------------------\n"
-    f"Winner: {winning_candidate}\n"
-    f"Winning Vote Count: {winning_count:,}\n"
-    f"Winning Percentage: {winning_percentage:.1f}%\n"
-    f"----------------------------\n"
-)
+    # Print the winning candidate's results to the terminal
+    winning_candidate_summary = (
+        f"----------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"----------------------------\n"
+    )
 
-print(winning_candidate_summary)
+    print(winning_candidate_summary)
 
-
-
-
-
-
-
-
-
-
-
-
+    # Save the winning candidate's results to the text file
+    txt_file.write(winning_candidate_summary)
 
 
-# close the file
 
-# To do: perform analysis
 
-# Close the file
 
-# The data we need to retrieve
 
-# 1. The total number of votes cast
 
-# 2. Complete list of candidates who received votes
 
-# 3. Percentage of votes each candidate won
 
-# 4. Total number of votes each candidate won
 
-# 5. Winner of the election based on popular vote
+
+
+
+
+    # close the file
+
+    # To do: perform analysis
+
+    # Close the file
+
+    # The data we need to retrieve
+
+    # 1. The total number of votes cast
+
+    # 2. Complete list of candidates who received votes
+
+    # 3. Percentage of votes each candidate won
+
+    # 4. Total number of votes each candidate won
+
+    # 5. Winner of the election based on popular vote
